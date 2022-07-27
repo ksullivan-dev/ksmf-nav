aa148756b2dae4942e8087f08e4f015039888a1a
Kyle Sullivan <kyle.sullivan@leaflink.com>
2022-07-27 12:04:31 -0400

    KSMF Nav POC Setup
    POC Setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="padding: 32px; outline: 1px solid red">
+    <AppNavigation />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import AppNavigation from "@/components/AppNavigation.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    components: {
+      AppNavigation,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li>
+      <router-link to="/">Home</router-link>
+    </li>
+    <li>
+      <router-link to="/orders">Orders</router-link>
+    </li>
+    <li>
+      <router-link to="/orders/sub">Orders > Sub</router-link>
+    </li>
+    <li>
+      <router-link to="/orders/nope">Orders > Nope</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "AppNavigation",
+  };
+</script>
